3a82a5e9d321d2d86e6dc04ac063964d02c56878
Brad Nissen <bradanissen@gmail.com>
2019-07-27 22:09:15 -0700

    add the sa as a module

--- configuration.tf
+++ configuration.tf
@@ -2,15 +2,21 @@ module "create_rg" {
   source = "./terraform-module-rg"
 }
 
-resource "azurerm_storage_account" "dev" {
-  name                     = "${var.sa_name}"
-  resource_group_name      = "${module.create_rg.rg_name}"
-  location                 = "${var.sa_location}"
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-
-  tags = {
-    environment = "dev"
-    owner       = "bradnissen"
-  }
+module "create_sa" {
+  source = "./terraform-module-sa"
+  resource_group_name     = "${module.create_rg.rg_name}" 
 }
+
+
+# resource "azurerm_storage_account" "dev" {
+#   name                     = "${var.sa_name}"
+#   resource_group_name      = "${module.create_rg.rg_name}"
+#   location                 = "${var.sa_location}"
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
+
+#   tags = {
+#     environment = "dev"
+#     owner       = "bradnissen"
+#   }
+# }
