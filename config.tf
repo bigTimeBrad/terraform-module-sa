ad935a3ad69a3abb462474e29ed931eb02e13883
Brad Nissen <bradanissen@gmail.com>
2019-07-27 22:24:08 -0700

    updating variable to take input

--- config.tf
+++ config.tf
@@ -1,6 +1,6 @@
 resource "azurerm_storage_account" "dev" {
   name                     = "${var.sa_name}"
-  resource_group_name      = "this-should-change-at-runtime"
+  resource_group_name      = "${var.sa_rg_name}"
   location                 = "${var.sa_location}"
   account_tier             = "Standard"
   account_replication_type = "GRS"
--- input.tf
+++ input.tf
@@ -7,3 +7,8 @@ variable "sa_location" {
   description = "The location where the resource group should be created"
   default     = "West US"
 }
+
+variable "sa_rg_name" {
+  description = "The location where the resource group should be created"
+  default     = "this-should-change-at-runtime"
+}
