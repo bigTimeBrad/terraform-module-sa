d39ae7a9cc0be69918655be1101ce616d1a05869
Brad Nissen <bradanissen@gmail.com>
2019-07-27 22:04:45 -0700

    code added

---
+++ input.tf
@@ -0,0 +1,9 @@
+variable "sa_name" {
+  description = "Specifies the name of the storage account."
+  default     = "storageaccountbantestdev"
+}
+
+variable "sa_location" {
+  description = "The location where the resource group should be created"
+  default     = "West US"
+}
